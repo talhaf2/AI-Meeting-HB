5de83a6cce9b970d81371ffbe511ff306aabfa62
talhaf2 <talha@prostructengineering.com>
2025-04-23 04:00:34 +0500

    parse url

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 // API routes
 app.use('/api', meetingRoutes);
